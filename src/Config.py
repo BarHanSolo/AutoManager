8c1378d86c7e6fffdf970fdbd999f31ed0438188
BarHanSolo <andrzej.lippa@gmail.com>
2019-03-19 19:57:28 +0100

    całość po raz 1

---
+++ src/Config.py
@@ -0,0 +1,24 @@
+Config = {
+    'game': {
+        'caption': 'Auto Manager',
+        'height': 600,
+        'width': 800,
+        'fps': 30,
+        'bumper_size': 25
+    },
+    'snake': {
+        'height': 20,
+        'width': 20,
+        'speed': 10
+    },
+    'apple': {
+        'width': 20,
+        'height': 20
+    },
+    'colors': {
+        'white': (255, 255, 255),
+        'black': (0, 0, 0),
+        'green': (0, 100, 0),
+        'red': (100, 0, 0)
+    }
+}
