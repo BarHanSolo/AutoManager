42d86487cd529593dd71e83e67f47eb33b56fc0b
BarHanSolo <andrzej.lippa@gmail.com>
2019-05-10 18:25:36 +0200

    menu added

--- firstwindow.py
+++ firstwindow.py
@@ -1,63 +1,91 @@
-import pygame
-import thorpy
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import font as tkfont
+from tkinter import messagebox
+from math import floor as mfloor
+from faker import Faker
+from PIL import Image, ImageDraw
+import pickle
+from colour import Color
+import colorsys
+import random
+saveName='sav1'
+windowWidth=800
+windowHeight=400
+buttonWidth=mfloor((windowWidth/7)/7.5)
+with open('sav1', 'rb') as f:
+    data = pickle.load(f)
+color0=data['playercolors'][0]
+color1=data['playercolors'][1]
+color2=data['playercolors'][2]
+color0hex='#%02x%02x%02x' % color0
+color1hex='#%02x%02x%02x' % color1
+color2hex='#%02x%02x%02x' % color2
 
-#Declaration of the application in which the menu is going to live.
-application = thorpy.Application(size=(1000, 500), caption='ThorPy stupid Example')
+class SampleApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title_font = tkfont.Font(family='Lato', size=18, weight="bold", slant="italic")
+        self.bgCol = color0hex
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-#Setting the graphical theme. By default, it is 'classic' (windows98-like).
-thorpy.theme.set_theme('human')
+        self.frames = {}
+        for F in (MenuMenu, MenuOptions):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.config(bg=color0hex, )
+            frame.grid(row=0, column=0, sticky="nsew")
 
-#Declaration of some elements...
-useless1 = thorpy.Element.make("This button is useless.\nAnd you can't click it.")
+        self.show_frame("MenuMenu")
 
-text = "This button also is useless.\nBut you can click it anyway."
-useless2 = thorpy.Clickable.make(text)
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
-draggable = thorpy.Draggable.make("Drag me!")
+    def save_game(self, save_name):
+        '''save all things'''
+        with open(save_name, 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        messagebox.showinfo(save_name, "Game saved")
 
-box1 = thorpy.make_ok_box([useless1, useless2, draggable])
-options1 = thorpy.make_button("Some useless things...")
-thorpy.set_launcher(options1, box1)
+    def load_game(self, save_name):
+        '''load all things'''
+        global data
+        with open(save_name, 'rb') as f:
+            data = pickle.load(f)
+        messagebox.showinfo(save_name, "Game loaded")
 
-text11 = "Jakis guzior"
-text12 = "jakis drugi guzior"
-mybut1 = thorpy.Clickable.make(text11)
-mybut2 = thorpy.Clickable.make(text12)
-box3 = thorpy.make_ok_box([mybut1, mybut2])
-box3.set_size(500)
-options11 = thorpy.make_button("jakies opcje")
-thorpy.set_launcher(options11, box3)
+    def open(self, windowname):
+        exec(compile(open(windowname, "rb").read(), windowname, 'exec'), )
 
-inserter = thorpy.Inserter.make(name="Tip text: ",
-                                value="This is a default text.",
-                                size=(150, 20))
+class MenuMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        tk.Button(self, text="New game", command =lambda: SampleApp.open(self, "secondwindow.py"), width = buttonWidth, bg=color0hex).grid(row=1, column=1, pady=10)
+        tk.Button(self, text="Load game", width = buttonWidth, bg=color0hex).grid(row=2, column=1, pady=10)
+        tk.Button(self, text="Options",  width = buttonWidth, bg=color0hex).grid(row=3, column=1, pady=10)
+        tk.Button(self, text="Exit game", command =lambda: sys.exit(), width = buttonWidth, bg=color0hex).grid(row=4, column=1, pady=10)
 
-file_browser = thorpy.Browser.make(path="C:/Users/", text="Please have a look.")
+    def opener(self):
+        with open("secondwindow.py") as f:
+            code = compile(f.read(), "somefile.py", 'exec')
+            exec(code)
 
-browser_launcher = thorpy.BrowserLauncher.make(browser=file_browser,
-                                                const_text="Choose a file: ",
-                                                var_text="")
+class MenuOptions(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
 
-color_setter = thorpy.ColorSetter.make()
-color_launcher = thorpy.ColorSetterLauncher.make(color_setter,
-                                                    "Launch color setter")
-
-options2 = thorpy.make_button("Useful things")
-box2 = thorpy.make_ok_box([inserter, color_launcher, browser_launcher])
-thorpy.set_launcher(options2, box2)
-
-quit_button = thorpy.make_button("Quit")
-quit_button.set_as_exiter()
-
-central_box = thorpy.Box.make([options1, options2, options11, quit_button])
-central_box.set_main_color((200, 200, 200, 120))
-central_box.center()
-
-#Declaration of a background element - include your own path!
-background = thorpy.Background.make(image=thorpy.style.EXAMPLE_IMG,
-                                    elements=[central_box])
-
-menu = thorpy.Menu(elements=background, fps=45)
-menu.play()
-
-application.quit()
+if __name__ == "__main__":
+    app = SampleApp()
+    app.title("AutoManager")
+    back = tk.Frame(master=app, width=windowWidth, height=windowHeight, bg=color0hex)
+    back.pack()
+    app.mainloop()
--- secondwindow.py
+++ secondwindow.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from tkinter import font  as tkfont
+from tkinter import font as tkfont
+from tkinter import messagebox
 from math import floor as mfloor
 from faker import Faker
 from PIL import Image, ImageDraw
@@ -9,12 +10,17 @@ import pickle
 from colour import Color
 import colorsys
 import random
+from firstwindow import MenuMenu, MenuOptions
+from inspect import getfullargspec
+import math
 #def callback():
 #    print("click!")
 saveName='sav1'
 windowWidth=800
 windowHeight=400
-with open(saveName, 'rb') as f:
+buttonWidth=mfloor((windowWidth/7)/7.5)
+trackname1="Tu na razie jest ściernisco ale będzie San Francisco"
+with open('sav1', 'rb') as f:
     data = pickle.load(f)
 color0=data['playercolors'][0]
 color1=data['playercolors'][1]
@@ -22,6 +28,8 @@ color2=data['playercolors'][2]
 color0hex='#%02x%02x%02x' % color0
 color1hex='#%02x%02x%02x' % color1
 color2hex='#%02x%02x%02x' % color2
+colorbgmenu='#ff2800'
+colortextmenu='#FCD112'
 
 class SampleApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -37,22 +45,38 @@ class SampleApp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StatsPage, CarPage, DriversPage, MoneyPage, WikiPage, RanksPage):
+        for F in (StatsPage, CarPage, DriversPage, MoneyPage, WikiPage, RanksPage, EscPage, MenuMenu, OptionsMenu, NewGameMenu, LoadGameMenu):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
-            frame.config(bg=color0hex, ) #color for entire frame
+            if "Menu" in page_name:
+                frame.config(bg=colorbgmenu, )
+            else:
+                frame.config(bg=color0hex, ) #color for entire frame
             # put all of the pages in the same location;
             # the one on the top of the stacking order
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("StatsPage")
+        self.show_frame("MenuMenu")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+
+    def save_game(self, save_name):
+        '''save all things'''
+        with open(save_name, 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        messagebox.showinfo(save_name, "Game saved")
+
+    def load_game(self, save_name):
+        '''load all things'''
+        global data
+        with open(save_name, 'rb') as f:
+            data = pickle.load(f)
+        messagebox.showinfo(save_name, "Game loaded")
 
 class Draw(tk.Frame):
     def colors(name, count):
@@ -61,24 +85,38 @@ class Draw(tk.Frame):
         fake.seed(name)
         my_dict['col0']=fake.hex_color()
         my_dict['col1']=fake.hex_color()
-        col0=Color(my_dict['col0'])
-        col1=Color(my_dict['col1'])
-        h,s,l = random.random(), 0.5 + random.random()/2.0, 0.4 + random.random()/5.0
+        h,s,l = random.random(), 0.5 + random.random()/2.0, random.random()
         r,g,b = [int(256*i) for i in colorsys.hls_to_rgb(h,l,s)]
         col0 = (r,g,b)
         col0 = '#%02x%02x%02x' % col0
         my_dict['col0']=col0
         col0=Color(my_dict['col0'])
-        #h,s,l = random.random(), 0.5 + random.random()/2.0, 0.4 + random.random()/5.0
-        #r,g,b = [int(256*i) for i in colorsys.hls_to_rgb(h,l,s)]
-        #col1 = (r,g,b)
-        #col1 = '#%02x%02x%02x' % col1
-        #col1=Color(my_dict['col1'])
-        collist = list(col0.range_to(Color(col1),10,))
+        h,s,l = random.random(), 0.5 + random.random()/2.0, random.random()
+        r,g,b = [int(256*i) for i in colorsys.hls_to_rgb(h,l,s)]
+        col1 = (r,g,b)
+        col1 = '#%02x%02x%02x' % col1
+        col1=Color(my_dict['col1'])
+        if "Ferrari" in name:
+            col0='#D40000'
+            my_dict['col0']=col0
+            col0=Color(my_dict['col0'])
+        elif "McLaren" in name:
+            col0='#c0c0c0'
+            my_dict['col0']=col0
+            col0=Color(my_dict['col0'])
+        elif "Jaguar" in name:
+            col0='#004225'
+            my_dict['col0']=col0
+            col0=Color(my_dict['col0'])
+        elif "Gordini" in name:
+            col0='#318CE7'
+            my_dict['col0']=col0
+            col0=Color(my_dict['col0'])
+        collist = list(col0.range_to(Color(col1),6,))
+        print(collist)
         for x in range(1,count):
             colname='col'+str(x+1)
             my_dict[colname]=collist[x].hex
-        print(my_dict)
         return my_dict
     def helmetDraw(name, canvas):
         from random import seed
@@ -136,20 +174,119 @@ class Draw(tk.Frame):
                     canvas.create_line(x,y,x+1,y, fill=col["col3"])
                 elif (tempcol=="#00ffff"):
                     canvas.create_line(x,y,x+1,y, fill=col["col4"])
+    def trackGen(name):
+        pixels=100
+        fake=Faker()
+        fake.seed(name)
+        trackTable=[]
+        for x in range(pixels):
+            for y in range(pixels):
+                num = fake.pydecimal(left_digits=3, right_digits=0, positive=True)
+                if num==0.0:
+                    trackTable.append(1)
+                else:
+                    trackTable.append(0)
+        return trackTable
+    def trackGen2(name):
+        pixels=100
+        from random import seed
+        from random import randint
+        seed(name)
+        curvenum=randint(11,21)
+        dots=[]
+        for x in range(curvenum):
+            dots.append([randint(0,pixels-1),randint(0,pixels-1)])
+        return dots
+    def clockwiseangle_and_distance(point):
+        origin=[60,70]
+        refvec = [0, 1]
+        # Vector between point and the origin: v = p - o
+        vector = [point[0]-origin[0], point[1]-origin[1]]
+        # Length of vector: ||v||
+        lenvector = math.hypot(vector[0], vector[1])
+        # If length is zero there is no angle
+        if lenvector == 0:
+            return -math.pi, 0
+        # Normalize vector: v/||v||
+        normalized = [vector[0]/lenvector, vector[1]/lenvector]
+        dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # x1*x2 + y1*y2
+        diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # x1*y2 - y1*x2
+        angle = math.atan2(diffprod, dotprod)
+        # Negative angles represent counter-clockwise angles so we need to subtract them
+        # from 2*pi (360 degrees)
+        if angle < 0:
+            return 2*math.pi+angle, lenvector
+        # I return first the angle because that's the primary sorting criterium
+        # but if two vectors have the same angle then the shorter distance should come first.
+        return angle, lenvector
+    def trackDraw(name, canvas):
+        dots=Draw.trackGen2(name)
+        #dots=[]
+        #for x in range(100):
+    #        for y in range(100):
+    #            if trackList[(x*100)+y]==1:
+    #                dots.append([x,y])
+        print(dots)
+        dots=sorted(dots, key=Draw.clockwiseangle_and_distance)
+        print(dots)
+        #key=lambda k: [k[0]+k[1],k[0]]
+        out = []
+        for x,y in dots:
+            out += [x*5, y*4]
+        canvas.create_polygon(out, fill='', outline='black', smooth=0, width=8, joinstyle=ROUND, splinesteps=5)
+
+class BackMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller=controller
+        Frame(self, bg=colortextmenu, height=5, width=windowWidth).grid(row=10, column=1, columnspan=7)
+        tk.Button(self, text="Back", command=lambda: controller.show_frame("MenuMenu"), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=11, column=2, pady=10)
+        tk.Button(self, text="OK", command=lambda: controller.show_frame("MenuMenu"), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=11, column=5, pady=10) #przyciski na dole z paskiem, back oraz ok
+
+class MenuMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        #tk.Label(self, text="", font=controller.title_font,bg=controller.bgCol).grid(sticky="W",row=1, column=1, columnspan=7, padx=10)
+        Frame(self, bg=colortextmenu, height=5, width=windowWidth).grid(row=0, column=1, columnspan=7)
+        tk.Button(self, text="New game", command =lambda: controller.show_frame("NewGameMenu"), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=1, column=4, pady=10)
+        tk.Button(self, text="Load game", command =lambda: controller.show_frame("LoadGameMenu"), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=2, column=4, pady=10)
+        tk.Button(self, text="Options", command =lambda: controller.show_frame("OptionsMenu"), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=3, column=4, pady=10)
+        tk.Button(self, text="Exit game", command =lambda: sys.exit(), width = buttonWidth, bg=colorbgmenu, fg=colortextmenu).grid(row=4, column=4, pady=10)
+        Frame(self, bg=colortextmenu, height=5, width=windowWidth).grid(row=10, column=1, columnspan=7)
+
+class OptionsMenu(BackMenu):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        BackMenu.__init__(self, parent, controller)
+
+class NewGameMenu(BackMenu):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        BackMenu.__init__(self, parent, controller)
+
+class LoadGameMenu(BackMenu):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        BackMenu.__init__(self, parent, controller)
 
 class MenuPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller=controller
-        buttonWidth=mfloor((windowWidth/6)/7.5)
+        buttonWidth=mfloor((windowWidth/7)/7.5)
         tk.Button(self, text="Stats", command=lambda: controller.show_frame("StatsPage"), width = buttonWidth, bg=color0hex, fg=color1hex).grid(row=0, column=1, pady=10)
         tk.Button(self, text="Drivers", command=lambda: controller.show_frame("DriversPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=2)
         tk.Button(self, text="Cars", command=lambda: controller.show_frame("CarPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=3)
         tk.Button(self, text="Money", command=lambda: controller.show_frame("MoneyPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=4)
         tk.Button(self, text="Ranks", command=lambda: controller.show_frame("RanksPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=5)
         tk.Button(self, text="Wiki", command=lambda: controller.show_frame("WikiPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=6)
-        Frame(self, bg=color2hex, height=5, width=windowWidth).grid(row=2, column=1, columnspan=6) #linia przerwy między menu, a resztą strony
-        Frame(self, bg=color2hex, height=5, width=windowWidth).grid(row=10, column=1, columnspan=6) #linia przerwy między resztą strony, a paskiem czasu
+        tk.Button(self, text="Menu", command=lambda: controller.show_frame("EscPage"), width = buttonWidth, bg=color0hex).grid(row=0, column=7)
+        Frame(self, bg=color2hex, height=5, width=windowWidth).grid(row=2, column=1, columnspan=7) #linia przerwy między menu, a resztą strony
+        Frame(self, bg=color2hex, height=5, width=windowWidth).grid(row=10, column=1, columnspan=7) #linia przerwy między resztą strony, a paskiem czasu, przyciski na górze
 
 class StatsPage(MenuPage):
     def __init__(self, parent, controller):
@@ -174,10 +311,11 @@ class DriversPage(MenuPage):
         self.controller = controller
         MenuPage.__init__(self, parent, controller)
         tk.Label(self, text="Drivers and Workers", font=controller.title_font,bg=controller.bgCol).grid(sticky="W",row=1, column=1, columnspan=7, padx=10)
-        self.helmet1=tk.Canvas(self, width=500, height=137, highlightthickness=0)
-        self.helmet1.grid(sticky="W",row=3, column=1, columnspan=7, padx=10)
-        self.helmet2=tk.Canvas(self, width=500, height=137, highlightthickness=0)
-        self.helmet2.grid(sticky="W",row=4, column=1, columnspan=7, padx=10)
+        tk.Label(self, text=trackname1, font=controller.title_font,bg=controller.bgCol).grid(sticky="W",row=3, column=1, columnspan=7, padx=10)
+        self.helmet1=tk.Canvas(self, width=500, height=500, highlightthickness=0)
+        self.helmet1.grid(sticky="W",row=4, column=1, columnspan=7, padx=10)
+        self.helmet2=tk.Canvas(self, width=500, height=200, highlightthickness=0)
+        self.helmet2.grid(sticky="W",row=5, column=1, columnspan=7, padx=10)
 
 class MoneyPage(MenuPage):
     def __init__(self, parent, controller):
@@ -200,11 +338,24 @@ class WikiPage(MenuPage):
         MenuPage.__init__(self, parent, controller)
         tk.Label(self, text="Wikipedia of Motorsport", font=controller.title_font,bg=controller.bgCol).grid(sticky="W",row=1, column=1, columnspan=7, padx=10)
 
+class EscPage(MenuPage):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        MenuPage.__init__(self, parent, controller)
+        buttonWidth=mfloor((windowWidth/7)/7.5)
+        tk.Label(self, text="Menu", font=controller.title_font,bg=controller.bgCol).grid(sticky="W",row=1, column=1, columnspan=7, padx=10)
+        tk.Button(self, text="Save", command=lambda: controller.save_game('sav1'), width = buttonWidth, bg=color0hex).grid(row=3, column=4)
+        tk.Button(self, text="Load", command=lambda: controller.load_game("EscPage"), width = buttonWidth, bg=color0hex).grid(row=4, column=4)
+        tk.Button(self, text="Options", command=lambda: controller.show_frame("EscPage"), width = buttonWidth, bg=color0hex).grid(row=5, column=4)
+        tk.Button(self, text="Exit to main menu", command =lambda: controller.show_frame("MenuMenu"), width = buttonWidth, bg=color0hex).grid(row=6, column=4)
+        tk.Button(self, text="Exit to OS", command=lambda: sys.exit(), width = buttonWidth, bg=color0hex).grid(row=7, column=4)
+
 if __name__ == "__main__":
     app = SampleApp()
     app.title("AutoManager")
-    Draw.carDraw("88888", app.frames['DriversPage'].helmet1)
-    Draw.carDraw("88887", app.frames['DriversPage'].helmet2)
+    #Draw.helmetDraw("Ferrario12345678", app.frames['DriversPage'].helmet1)
+    Draw.trackDraw(trackname1, app.frames['DriversPage'].helmet1)
     back = tk.Frame(master=app, width=windowWidth, height=windowHeight, bg=color0hex)
     back.pack()
     app.mainloop()
